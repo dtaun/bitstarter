6a152abb8080afc00543d0667d310631b53f125f
David Taun <dtaun1@gmail.com>
2013-07-12 18:07:08 +0000

    Deleted useless stuff

--- web.js
+++ web.js
@@ -8,9 +8,7 @@ var data = fs.readFileSync("index.html", "utf8");
 app.get('/', function(request, response) {
   response.send(data);
 });
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
